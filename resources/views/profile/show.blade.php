729000bb8ca5b5b1dccad5a5ab80447c90668756
gmaaliki <maalikighifari@gmail.com>
2023-11-25 20:56:37 +0700

    remove more dark mode

--- resources/views/profile/show.blade.php
+++ resources/views/profile/show.blade.php
@@ -1,6 +1,6 @@
 <x-app-layout>
     <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
+        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
             {{ __("Profile") }}
         </h2>
     </x-slot>
@@ -9,9 +9,9 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="flex justify-center overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="flex-col w-2/5 m-8">
-                    <div class="flex-col align-center bg-gray-800 dark:bg-white p-4">
+                    <div class="flex-col align-center bg-gray-800 p-4">
                         <!-- Make this a component later -->
-                        <img class="w-40 h-40 rounded-full bg-white border-2 dark:border-black mx-auto" src="https://www.svgrepo.com/show/396909/letter-s.svg" alt="Rounded avatar">
+                        <img class="w-40 h-40 rounded-full bg-white border-2 mx-auto" src="https://www.svgrepo.com/show/396909/letter-s.svg" alt="Rounded avatar">
                         <div class="text-3xl">
                             <h1 class="text-center">{{ Auth::user()->name }}</h1>
                         </div>
@@ -34,7 +34,7 @@
                             </div>
                         </div>
                     </div>    
-                    <div class="flex-col align-center bg-gray-800 dark:bg-white p-4 my-5">
+                    <div class="flex-col align-center bg-gray-800 p-4 my-5">
                         <div class="flex font-semibold">
                             <div class="w-1/2 text-left">
                                 <h3>Languages</h3>
@@ -48,7 +48,7 @@
                             <p>English</p>
                         </div>
                     </div>
-                    <div class="flex-col align-center bg-gray-800 dark:bg-white p-4 my-5">
+                    <div class="flex-col align-center bg-gray-800 p-4 my-5">
                         <div class="flex font-semibold">
                             <div class="w-1/2 text-left">
                                 <h3>Skills</h3>
@@ -62,7 +62,7 @@
                             <p>Gardening</p>
                         </div>
                     </div>
-                    <div class="flex-col align-center bg-gray-800 dark:bg-white p-4 my-5">
+                    <div class="flex-col align-center bg-gray-800 p-4 my-5">
                         <div class="flex font-semibold">
                             <div class="w-1/2 text-left">
                                 <h3>Education</h3>
@@ -76,7 +76,7 @@
                             <p>High School</p>
                         </div>
                     </div>
-                    <div class="flex-col align-center bg-gray-800 dark:bg-white p-4 my-5">
+                    <div class="flex-col align-center bg-gray-800 p-4 my-5">
                         <div class="flex font-semibold">
                             <div class="w-1/2 text-left">
                                 <h3>Certification</h3>
@@ -92,7 +92,7 @@
                     </div>
                 </div>
                 <div class="flex-col w-3/5 m-8">
-                    <div class="flex-col align-center font-semibold text-3xl bg-gray-800 dark:bg-white p-4">
+                    <div class="flex-col align-center font-semibold text-3xl bg-gray-800 p-4">
                         {{ __("Gigs list") }}
                         <x-primary-button>
                             {{ __("Add gig") }}
